42c15e01aab3d1d7fbdfc16902e0337fc316114e
Devin <aebid@tamu.edu>
2022-05-10 00:15:28 +0200

    Updated to loop over both ME11 and GE11 results

--- GEM_Alignment/script/plotting/globalshift.py
+++ GEM_Alignment/script/plotting/globalshift.py
@@ -1,66 +1,36 @@
-import ROOT, os
+import ROOT, os, sys
 from make_plot import *
 
-#f = ROOT.TFile("../condor/run342218_jun30/out_run342218_jun30.root")
-#f = ROOT.TFile("/afs/cern.ch/user/t/toakhter/public/GEMAl2018Cv4.root")
-#f = ROOT.TFile("/afs/cern.ch/work/d/daebi/analyser/CMSSW_11_3_0_pre5/src/GEMCSCBendingAnalyzer/MuonAnalyser/condor/MWGR4_condor/run342218_jul12/out_run342218_jul14.root")
-#f = ROOT.TFile("out_ZeroBias_testbeam_nov12.root")
-f = ROOT.TFile("out_ME11ana_CRAFT2022_ExpressCosmics_ME11Iter2_Align.root")
-
-#event = f.Get("analyser/MuonData")
-#event_ME11Seg = f.Get("analyser/ME11Seg_Prop")
-#event_CSC = f.Get("analyser/CSC_Prop")
-event_ME11ana = f.Get("ME11ana/Inner_Prop")
-
-
+#fname = "out_RdPhiAna_Craft2022_TerukiRuns_ME11Iter0_GEMIter0_080522.root"
+fname = "{}".format(sys.argv[1])
+trees = ["ME11ana/Inner_Prop", "analyzer/ME11Seg_Prop"]
 RdPhi_Low = -1
 RdPhi_High = 1
+cut = "abs(RdPhi) < 5 && muon_pt > 3"
 
-h1 = plot1Dprofile("RdPhi:prop_location[3]", "ME11 RdPhi Profile CRAFT2022 R-1", event_ME11ana, [36, 1, 37, 10, RdPhi_Low, RdPhi_High], "Chamber", "RdPhi", "abs(RdPhi) < 20 && prop_location[0] == -1", "RdPhi", True)
-c1 = ROOT.TCanvas("", "", 800, 600)
-hist = ROOT.TH1D("h1", "ME11 RdPhi Profile CRAFT2022 R-1", 36, -(3.14159265)/36., 2*3.14159265 - (3.14159265)/36.)
-for i in range(1, 37):
-  hist.SetBinContent(i, h1.GetBinContent(i))
+f = ROOT.TFile(fname)
+for tree in trees:
+  event = f.Get(tree)
+  branch = "RdPhi"
+  if "ME11ana" in tree: branch = branch+":prop_location[3]"
+  if "analyzer" in tree: branch = branch + "_Corrected:prop_location[2]"
+  plotname = ""
+  if "ME11ana" in tree: plotname = "ME11_CRAFT2022"
+  if "analyzer" in tree: plotname = "GE11_CRAFT2022"
 
-hist.GetYaxis().SetRangeUser(RdPhi_Low, RdPhi_High)
-hist.GetXaxis().SetTitle("Phi")
-hist.GetYaxis().SetTitle("RdPhi [cm]")
-hist.Sumw2()
-hist.Draw("Hist")
-f1 = ROOT.TF1("f1", "[0]*sin(x) + [1]*cos(x) + [2]", 0, 2*3.14159265)
-f1.SetParameters(.1, .1, 0)
-hist.Fit("f1")
-f1.Draw("same")
-hist.SetStats(False)
-latex = ROOT.TLatex()
-latex.SetTextAlign(12)
-latex.SetTextSize(0.04)
-latex.SetTextFont(61)
-latex.DrawLatex(2, 0.6, "{zer} sin(x) + {one} cos(x) + {two}".format(zer = round(f1.GetParameter(0), 4), one = round(f1.GetParameter(1), 4), two = round(f1.GetParameter(2), 4)))
-c1.SaveAs("ME11_R-1_GlobalShift.png")
+  for reg in [-1, 1]:
+    h1 = plot1Dprofile(branch, "{plotname} RdPhi Profile R{reg}".format(plotname = plotname, reg = reg), event, [36, 1, 37, 10, -100, 100], "Chamber", "RdPhi", cut + " && prop_location[0] == {reg}".format(reg = reg), "RdPhi", False)
+    c1 = ROOT.TCanvas("", "", 800, 600)
+    hist = ROOT.TH1D("{plotname} RdPhi Profile R{reg}".format(plotname = plotname, reg = reg), "{plotname} RdPhi Profile R{reg}".format(plotname = plotname, reg = reg), 36, -(3.14159265)/36., 2*3.14159265 - (3.14159265)/36.)
+    for i in range(1, 37):
+      hist.SetBinContent(i, h1.GetBinContent(i))
 
-
-
-h1 = plot1Dprofile("RdPhi:prop_location[3]", "ME11 RdPhi Profile CRAFT2022 R1", event_ME11ana, [36, 1, 37, 10, RdPhi_Low, RdPhi_High], "Chamber", "RdPhi", "abs(RdPhi) < 20 && prop_location[0] == 1", "RdPhi", True)
-
-c1 = ROOT.TCanvas("", "", 800, 600)
-hist = ROOT.TH1D("h1", "ME11 RdPhi Profile CRAFT2022 R1", 36, -(3.14159265)/36., 2*3.14159265 - (3.14159265)/36.)
-for i in range(1, 37):
-  hist.SetBinContent(i, h1.GetBinContent(i))
-
-hist.GetYaxis().SetRangeUser(RdPhi_Low, RdPhi_High)
-hist.GetXaxis().SetTitle("Phi")
-hist.GetYaxis().SetTitle("RdPhi [cm]")
-hist.Sumw2()
-hist.Draw("Hist")
-f1 = ROOT.TF1("f1", "[0]*sin(x) + [1]*cos(x) + [2]", 0, 2*3.14159265)
-f1.SetParameters(1, 1, 0)
-hist.Fit("f1")
-f1.Draw("same")
-hist.SetStats(False)
-latex = ROOT.TLatex()
-latex.SetTextAlign(12)
-latex.SetTextSize(0.04)
-latex.SetTextFont(61)
-latex.DrawLatex(2, 0.6, "{zer} sin(x) + {one} cos(x) + {two}".format(zer = round(f1.GetParameter(0), 4), one = round(f1.GetParameter(1), 4), two = round(f1.GetParameter(2), 4)))
-c1.SaveAs("ME11_R1_GlobalShift.png")
+    hist.GetYaxis().SetRangeUser(RdPhi_Low, RdPhi_High)
+    hist.GetXaxis().SetTitle("Phi")
+    hist.GetYaxis().SetTitle("RdPhi [cm]")
+    hist.Sumw2()
+    hist.Draw("Hist")
+    hist.SetStats(False)
+    c1.SaveAs("{plotname}_R{reg}_GlobalShift.png".format(plotname = plotname, reg = reg))
+    del h1
+    del hist
