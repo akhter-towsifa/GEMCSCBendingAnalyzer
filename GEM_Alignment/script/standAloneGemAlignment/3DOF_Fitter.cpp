8c3664e0a796d97ffd15a62d2af005eca27b404e
Towsifa Akhter <towsifa.akhter@gmail.com>
2023-02-09 01:48:10 +0100

    fitter minor change

--- GEM_Alignment/script/standAloneGemAlignment/3DOF_Fitter.cpp
+++ GEM_Alignment/script/standAloneGemAlignment/3DOF_Fitter.cpp
@@ -104,12 +104,12 @@ int main() {
   //////////////////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////////////////
   //Input root file name
-  const char* input_name = "../../test/Run2022D_ZMu_PromptReco_RAWRECO_globalMu_pfisotight_v7.root";
+  const char* input_name = "../../test/singleMuonGun_11_3_4_2021_design_idealGEMmisalignedME11_dx1cm.root";
   //Tree name ***Make sure to use correct one***
   const char* tree_name = "analyzer/ME11Seg_Prop";   //"analyzer/ME11Seg_Prop" or "ME11ana/Inner_Prop" for example
   const char* Rdphi_name = "RdPhi";
   //Will only change the name of the output csv file
-  const char* outname_prefix = "2022D_globalMu_pfisotight_v7_ME11Seg_Prop";
+  const char* outname_prefix = "GEMvalues_misalignedME11_dx1cm";
   //Cuts on full tree in first cloning step
   const char* cuts = "muon_pt > 5 && abs(RdPhi) < 100 && has_fidcut"; //n_ME11_segment == 1
   //Option to turn on or off 3 dof alignments and layer level vs chamber level
@@ -160,8 +160,10 @@ int main() {
     //Finished base cloning, create output CSV "GE11_out_CutNumber*.csv"
     std::cout << "New number of entries = " << cutEn->GetEntries() << std::endl;
     std::ofstream myfile;
-    std::cout << "Creating CSV file " << Form("%s_out_CutNumber%d.csv", outname_prefix, nCut) << std::endl;
-    myfile.open (Form("%s_out_CutNumber%d.csv", outname_prefix, nCut));
+    std::cout << "Creating CSV file " << Form("%s.csv", outname_prefix) << std::endl;
+    myfile.open (Form("%s.csv", outname_prefix));
+    //std::cout << "Creating CSV file " << Form("%s_out_CutNumber%d.csv", outname_prefix, nCut) << std::endl;
+    //myfile.open (Form("%s_out_CutNumber%d.csv", outname_prefix, nCut));
     double dx, dy, dz, dphix, dphiy, dphiz;
     int detNum;
     dz = 0.0; dphix = 0.0; dphiy = 0.0;
@@ -178,7 +180,7 @@ int main() {
           TFile* tmpTF = new TFile("tmp2.root","recreate");
           std::cout <<"About to copy tree" << std::endl;
           TTree* tt_tmp;
-          if (j==-1 and i==25){continue;}
+          //if (j==-1 and i==25){continue;}
           if(byLayer){
             tt_tmp = cutEn->CopyTree(Form("rechit_detId==%d && prop_location[3] == %d",detNum, k));		//Only fits 1 chamber at a time (det_id)
           }
