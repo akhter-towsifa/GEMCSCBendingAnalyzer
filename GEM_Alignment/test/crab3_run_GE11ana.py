15a5d0aabd5d157da9bfa20fa59b6ba8c48d3eea
Towsifa Akhter <towsifa.akhter@gmail.com>
2025-06-02 19:20:15 +0200

    2025B

--- GEM_Alignment/test/crab3_run_GE11ana.py
+++ GEM_Alignment/test/crab3_run_GE11ana.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config
 config = config()
 
 #section General
-config.General.requestName = 'Run2025B_muon0_ZMu_150X_dataRun3_Prompt_v1_aligned' 
+config.General.requestName = 'Run2025B_muon0_ZMu_150X_dataRun3_Prompt_v1' 
 config.General.workArea = 'crabLogs'#working directory
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -12,12 +12,12 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'run_GE11ana.py'
 config.JobType.numCores = 1
 
-misalign = True  #Make sure to change the run_GE11ana.py too!!!
+misalign = False  #Make sure to change the run_GE11ana.py too!!!
 if misalign:
-  config.JobType.inputFiles =  ['./Run2025B_muon0_ZMu_150X_dataRun3_Prompt_v1.db']
+  config.JobType.inputFiles =  ['']
 
 #section Data
-config.Data.runRange = '391848-392112'
+# config.Data.runRange = '392280-392700'
 config.Data.inputDataset = '/Muon0/Run2025B-ZMu-PromptReco-v1/RAW-RECO'
 # config.Data.userInputFiles = open('singleMuonGun_11_3_4_2021_design.list').readlines()
 # config.Data.lumiMask = '/eos/user/c/cmsdqm/www/CAF/certification/Collisions23/Cert_Collisions2023_366442_370790_Muon.json'
